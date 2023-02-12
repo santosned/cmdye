1dd29ebdbfe3bdb2a6a22b7380d91a7e83d50d24
E. M. Santos <santosned@proton.me>
2023-02-11 23:29:18 -0300

    build: upload Typescript and Rollup configs

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'package/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/index.mjs',
+      format: 'esm',
+    },
+  ],
+  plugins: [
+    typescript({
+      compilerOptions: { declaration: false, emitDeclarationOnly: false },
+    }),
+    terser({
+      ecma: 5,
+      module: true,
+      toplevel: true,
+      compress: { pure_getters: true },
+      format: { quote_style: 'original' },
+    }),
+  ],
+};
