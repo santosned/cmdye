63099e0e1a2d5fb704088d23cb87e8abda9efeda
E. M. Santos <santosned@proton.me>
2023-02-19 20:48:16 -0300

    refactor(utils): split parser in smaller functions
    This decrease the amout of unrequired repetitions.

--- package/utils/parser.ts
+++ package/utils/parser.ts
@@ -3,28 +3,32 @@ import create8BitEscapeCode from './create8BitEscapeCode';
 
 import { AnyCodes } from '../types';
 
+const getEscapeCode = (value: string) => {
+  if (!charCodes.has(value)) {
+    throw new Error(`The value '${value}' doesn't exist in '${Array.from(charCodes.keys()).join(',')}'`);
+  }
+
+  return create8BitEscapeCode(charCodes.get(value)!);
+};
+
+const parserStringOptions = (anyCodes: string[]) => {
+  let codes = '';
+
+  for (const code of anyCodes) {
+    if (!code) continue;
+
+    codes += getEscapeCode(code);
+  }
+
+  return codes;
+};
+
 export const parser = (anyCodes: AnyCodes, match = ''): string => {
   if (!anyCodes) return match;
 
   if (!anyCodes.includes(',')) {
-    if (!charCodes.has(anyCodes)) {
-      throw new Error(`The value '${anyCodes}' doesn't exist in '${Array.from(charCodes.keys()).join(',')}'`);
-    }
-
-    return create8BitEscapeCode(charCodes.get(anyCodes)!);
+    return getEscapeCode(anyCodes);
   }
 
-  let escapeCodes = '';
-  const values = anyCodes.split(',');
-  for (const value of values) {
-    if (!value) continue;
-
-    if (!charCodes.has(value)) {
-      throw new Error(`The value '${value}' doesn't exist in '${Array.from(charCodes.keys()).join(',')}'`);
-    }
-
-    escapeCodes += create8BitEscapeCode(charCodes.get(value)!);
-  }
-
-  return escapeCodes;
+  return parserStringOptions(anyCodes.split(','));
 };
